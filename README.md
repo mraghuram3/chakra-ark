1c137318b4c140301175a1d1cabc12a0404c0412
raghu <19218859+mraghuram3@users.noreply.github.com>
2024-08-08 11:56:04 +0530

    updated read me

---
+++ README.md
@@ -0,0 +1,29 @@
+# ark
+
+### React + TypeScript + Vite + ARK + custom css
+
+Using Ark headless components with our own css
+
+---
+
+# ark-panda
+
+### React + TypeScript + Vite + ARK + panda css
+
+Using Ark headless components with panda css from creators of ark ui
+
+---
+
+# ark-tailwind
+
+### React + TypeScript + Vite + ARK + tailwind
+
+Using Ark headless components with tailwind css
+
+---
+
+# park-ui
+
+### React + TypeScript + Vite + park-ui
+
+Using Park ui which is built with ark headless components and pandcss.
--- park-ui/src/pages/button.tsx
+++ park-ui/src/pages/button.tsx
@@ -1,65 +1,60 @@
+import { ArrowRightIcon } from "lucide-react";
 
-import { ArrowRightIcon } from 'lucide-react'
-import { Button,  } from '~/components/ui/button'
+import { Button } from "~/components/ui/button";
 
 import { css } from "../../styled-system/css";
 
-
 const BUTTON_SIZES = {
-    TINY: "xs",
-    SMALL: "sm",
-    MEDIUM: "md",
-    LARGE: "lg"
-}
+  TINY: "xs",
+  SMALL: "sm",
+  MEDIUM: "md",
+  LARGE: "lg",
+};
 
 const BUTTON_VARIANTS = {
-    SOLID: "solid",
-    SUBTLE: "subtle",
-    OUTLINE: "outline",
-    GHOST: "outline"
-}
+  SOLID: "solid",
+  SUBTLE: "subtle",
+  OUTLINE: "outline",
+  GHOST: "outline",
+};
 
 export const ButtonPage = () => {
   return (
-
     <div
-    className={css({
+      className={css({
         display: "grid",
         justifyContent: "center",
         alignItems: "center",
         gap: "12px",
-        background:"white"
+        background: "white",
       })}
     >
-        <ButtonSizeComp />
-        <ButtonTypeComp />
+      <ButtonSizeComp />
+      <ButtonTypeComp />
     </div>
-  )
-}
+  );
+};
 
 export const ButtonSizeComp = () => {
-    return (
-      <div className="grid justify-items-start gap-20">
-        <p className="small">Size Variants</p>
-        <Button size={BUTTON_SIZES.TINY}>Button</Button>
-        <Button size={BUTTON_SIZES.SMALL}>Button</Button>
-        <Button size={BUTTON_SIZES.MEDIUM}>Button</Button>
-        <Button size={BUTTON_SIZES.LARGE}>Button</Button>
-      </div>
-    );
-  };
-  
-  export const ButtonTypeComp = () => {
-    return (
-      <div className="grid justify-items-start gap-20">
-        <p className="small">Type Variants</p>
-        <Button variant={BUTTON_VARIANTS.SOLID}>Button</Button>
-        <Button variant={BUTTON_VARIANTS.SUBTLE}>Button</Button>
-        <Button variant={BUTTON_VARIANTS.OUTLINE}>Button</Button>
-        <Button variant={BUTTON_VARIANTS.GHOST}>
-          Button
-        </Button>
-      </div>
-    );
-  };
-  
+  return (
+    <div className="grid justify-items-start gap-20">
+      <p className="small">Size Variants</p>
+      <Button size={BUTTON_SIZES.TINY}>Button</Button>
+      <Button size={BUTTON_SIZES.SMALL}>Button</Button>
+      <Button size={BUTTON_SIZES.MEDIUM}>Button</Button>
+      <Button size={BUTTON_SIZES.LARGE}>Button</Button>
+    </div>
+  );
+};
+
+export const ButtonTypeComp = () => {
+  return (
+    <div className="grid justify-items-start gap-20">
+      <p className="small">Type Variants</p>
+      <Button variant={BUTTON_VARIANTS.SOLID}>Button</Button>
+      <Button variant={BUTTON_VARIANTS.SUBTLE}>Button</Button>
+      <Button variant={BUTTON_VARIANTS.OUTLINE}>Button</Button>
+      <Button variant={BUTTON_VARIANTS.GHOST}>Button</Button>
+    </div>
+  );
+};
